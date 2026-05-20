e71a5001a10baa02d0d22cd61e0dc501ada4c7b0
Enrique <e_solis83@yahoo.com>
2026-05-19 17:07:29 -0700

    Testing

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -14,7 +14,7 @@ export function NavBar() {
   return (
     <nav className={styles.nav}>
       <Link to="/" className={styles.logo}>
-        FRAMESHIFT
+        FRAMESHIFTs
       </Link>
       <form onSubmit={handleSearch} className={styles.searchForm}>
         <input
